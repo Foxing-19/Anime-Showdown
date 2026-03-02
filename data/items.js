bb3a6a785e8c211225d54984137cff4f91af8e61
Foxing-19 <foxing898@gmail.com>
2026-03-02 12:16:48 -0300

    Create items.js

---
+++ data/items.js
@@ -0,0 +1,168 @@
+module.exports = {
+
+Leftovers:{
+desc:"Heal 1/16 HP every turn"
+},
+
+ChoiceBand:{
+desc:"1.5x Attack but locked into one move"
+},
+
+ChoiceSpecs:{
+desc:"1.5x Special Attack but locked into one move"
+},
+
+ChoiceScarf:{
+desc:"1.5x Speed but locked into one move"
+},
+
+FocusSash:{
+desc:"Survive a hit with 1 HP if at full HP"
+},
+
+LifeOrb:{
+desc:"Boost damage by 30% but lose HP"
+},
+
+RockyHelmet:{
+desc:"Damages attacker on contact"
+},
+
+AssaultVest:{
+desc:"Boost SpDef but cannot use status moves"
+},
+
+ExpertBelt:{
+desc:"Boost super effective moves"
+},
+
+MuscleBand:{
+desc:"Boost physical moves"
+},
+
+WiseGlasses:{
+desc:"Boost special moves"
+},
+
+BlackBelt:{
+desc:"Boost Fighting moves"
+},
+
+MysticWater:{
+desc:"Boost Water moves"
+},
+
+Charcoal:{
+desc:"Boost Fire moves"
+},
+
+Magnet:{
+desc:"Boost Electric moves"
+},
+
+NeverMeltIce:{
+desc:"Boost Ice moves"
+},
+
+PoisonBarb:{
+desc:"Boost Poison moves"
+},
+
+SharpBeak:{
+desc:"Boost Flying moves"
+},
+
+SoftSand:{
+desc:"Boost Ground moves"
+},
+
+SilverPowder:{
+desc:"Boost Bug moves"
+},
+
+SpellTag:{
+desc:"Boost Ghost moves"
+},
+
+TwistedSpoon:{
+desc:"Boost Psychic moves"
+},
+
+HardStone:{
+desc:"Boost Rock moves"
+},
+
+MetalCoat:{
+desc:"Boost Steel moves"
+},
+
+DragonFang:{
+desc:"Boost Dragon moves"
+},
+
+BlackGlasses:{
+desc:"Boost Dark moves"
+},
+
+FairyFeather:{
+desc:"Boost Fairy moves"
+},
+
+QuickClaw:{
+desc:"Chance to move first"
+},
+
+BrightPowder:{
+desc:"Raises evasion"
+},
+
+ScopeLens:{
+desc:"Raises critical hit chance"
+},
+
+RazorClaw:{
+desc:"Boost crit rate"
+},
+
+KingsRock:{
+desc:"Chance to flinch"
+},
+
+WideLens:{
+desc:"Boost move accuracy"
+},
+
+ZoomLens:{
+desc:"Boost accuracy if moving last"
+},
+
+ShellBell:{
+desc:"Recover HP from damage dealt"
+},
+
+AirBalloon:{
+desc:"Immune to Ground until hit"
+},
+
+EjectButton:{
+desc:"Switch out when hit"
+},
+
+RedCard:{
+desc:"Force opponent switch"
+},
+
+WeaknessPolicy:{
+desc:"Boost Atk and SpA if hit super effectively"
+},
+
+ThroatSpray:{
+desc:"Boost SpA after sound move"
+},
+
+TerrainSeed:{
+desc:"Boost Defense in terrain"
+
+}
+
+}
