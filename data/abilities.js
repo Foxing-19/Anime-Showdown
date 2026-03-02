65e7a8d79285f0589bf6c84fbc2d0649a522e8a1
Foxing-19 <foxing898@gmail.com>
2026-03-02 12:20:16 -0300

    Create abilities.js

---
+++ data/abilities.js
@@ -0,0 +1,236 @@
+module.exports = {
+
+// === Dragon Ball ===
+UltraInstinct:{
+desc:"Boost evasion when HP is below 50%"
+},
+SaiyanPride:{
+desc:"Boost Attack when HP is full"
+},
+Zenkai:{
+desc:"Increase Attack after fainting an opponent"
+},
+GalickGun:{
+desc:"Special moves deal 10% more damage"
+},
+FinalFlash:{
+desc:"Boost SpA after each KO"
+},
+InstantTransmission:{
+desc:"Chance to move first"
+},
+KiOverload:{
+desc:"Increase SpA when HP is low"
+},
+LegendaryPower:{
+desc:"Boost all stats slightly at start"
+},
+SuperSaiyan:{
+desc:"Boost Atk and SpA when HP < 50%"
+},
+FusedWarrior:{
+desc:"If paired with another DB character, gain +Speed"
+},
+SaiyanResilience:{
+desc:"Heal 1/16 HP each turn"
+},
+EnergyAbsorb:{
+desc:"Recover 25% HP when hit by Special move"
+},
+LimitBreaker:{
+desc:"Boost Atk after surviving a KO move"
+},
+ZenkaiBoost:{
+desc:"Increase Speed after KOing opponent"
+},
+Overdrive:{
+desc:"Increase all offensive stats after receiving a debuff"
+},
+
+// === Naruto ===
+NineTails:{
+desc:"Boost Attack at low HP"
+},
+Sharingan:{
+desc:"Increase evasion against status moves"
+},
+Mangekyo:{
+desc:"Chance to flinch opponent after Special move"
+},
+SageMode:{
+desc:"Boost SpA and Speed in battle"
+},
+ChidoriSurge:{
+desc:"Electric moves deal +10% damage"
+},
+RasenganMaster:{
+desc:"Boost Fighting moves"
+},
+ShadowClone:{
+desc:"Once per battle, copy opponent's last move"
+},
+ByakuganVision:{
+desc:"Never miss attacks"
+},
+SealingTechnique:{
+desc:"Reduces opponent's HP by 10% after KOing"
+},
+TaijutsuFocus:{
+desc:"Boost physical moves"
+},
+HealingChakra:{
+desc:"Recover HP every turn"
+},
+TailedBeastMode:{
+desc:"Boost all stats at start"
+},
+FireReleaseMaster:{
+desc:"Boost Fire-type moves"
+},
+WaterReleaseMaster:{
+desc:"Boost Water-type moves"
+},
+WindReleaseBoost:{
+desc:"Boost Flying moves"
+},
+
+// === One Piece ===
+HakiMaster:{
+desc:"Boost Fighting moves"
+},
+GumGumGear:{
+desc:"Boost physical moves after using 1 move"
+},
+SwordMaster:{
+desc:"Boost Sword-type moves"
+},
+PirateWill:{
+desc:"Increase all stats slightly at start"
+},
+DevilFruitPower:{
+desc:"Boost Type advantage moves"
+},
+ObservationHaki:{
+desc:"Chance to move first"
+},
+ArmamentHaki:{
+desc:"Reduce damage from physical attacks"
+},
+GomuGomuNoPistol:{
+desc:"Boost Attack moves"
+},
+HakiConqueror:{
+desc:"Chance to flinch opponent at start"
+},
+RedHairedLegacy:{
+desc:"Boost Speed and Attack at start"
+},
+KatakuriObservation:{
+desc:"Immune to critical hits"
+},
+CrocodileSand:{
+desc:"Ground moves deal +10% damage"
+},
+MihawkPrecision:{
+desc:"Boost critical hits"
+},
+YonkoAura:{
+desc:"Increase HP and Attack slightly"
+},
+AceFlame:{
+desc:"Fire moves deal +10% damage",
+
+// === Bleach ===
+},
+Bankai:{
+desc:"Boost Attack and Speed"
+},
+GetsugaMaster:{
+desc:"Boost Special moves"
+},
+ZanpakutoBond:{
+desc:"Reduce damage received from physical moves"
+},
+KidoExpert:{
+desc:"Boost Ghost and Psychic moves"
+},
+ShinigamiFocus:{
+desc:"Increase Speed after KO"
+},
+HollowAwakening:{
+desc:"Boost Atk and SpA when HP < 50%"
+},
+ShunpoMaster:{
+desc:"Move first with high Speed"
+},
+EspadaPower:{
+desc:"Boost Attack of Dark moves"
+},
+SoulReaperForm:{
+desc:"Recover 10% HP after KOing"
+},
+Resurrection:{
+desc:"Boost all stats for 3 turns"
+},
+FlashStep:{
+desc:"Increase evasion for 2 turns"
+},
+IceRelease:{
+desc:"Boost Ice moves"
+},
+FireRelease:{
+desc:"Boost Fire moves"
+},
+BankaiOverdrive:{
+desc:"Once per battle, boost all stats"
+},
+ShikaiMaster:{
+desc:"Boost Attack moves",
+
+// === Jujutsu Kaisen ===
+SixEyes:{
+desc:"Reduces energy cost of moves"
+},
+Limitless:{
+desc:"Boost SpA and Atk after using a move"
+},
+DomainExpansion:{
+desc:"Boost all moves in one turn"
+},
+CursedEnergyControl:{
+desc:"Reduce damage from special moves"
+},
+ReverseCursedTechnique:{
+desc:"Heal 25% HP once per battle"
+},
+BlackFlash:{
+desc:"Boost physical and special moves"
+},
+CursedSpiritBond:{
+desc:"Reduce opponent's Attack by 10%"
+},
+HeavenlyRestriction:{
+desc:"Boost Speed when HP < 50%"
+},
+ShikigamiMaster:{
+desc:"Boost Ghost moves"
+},
+TenShadows:{
+desc:"Boost physical moves for 3 turns"
+},
+VengefulCurse:{
+desc:"Chance to flinch opponent after KO"
+},
+ProtectionCursedTool:{
+desc:"Reduce damage received once per battle"
+},
+MaximumDomain:{
+desc:"All moves boosted for 2 turns"
+},
+InfinityTechnique:{
+desc:"Boost SpA moves by 15%"
+},
+SukunaOverpower:{
+desc:"Boost Atk and SpA when HP < 50%"
+
+}
